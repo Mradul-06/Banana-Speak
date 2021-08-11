2c686504770aaa3df74a0da8501d77ce51f3ff99
Mradul Agrawal <mradulagrawal126@gmail.com>
2021-08-11 10:53:19 +0530

    completed

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-var btntranslate = document.querySelector("#btn-translate");
-var txtinput = document.querySelector("#txt-input");
-var outputdiv=document.querySelector("#output");
-
-// outputdiv.innertext ="mradul"
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 
-function clickHandler()
-{
-    console.log(txtinput.value)
-    outputdiv.innerText= txtinput.value;
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value; 
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; 
+           })
+        .catch(errorHandler)
 };
 
-
-btntranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickHandler)
